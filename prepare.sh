2830417396229ffd3c6a46626db5d7a0aec7ca73
Xavier Ruiz <xavier.ruiz@cronologic.com>
2018-10-12 21:57:37 +0200

    ejecute vi to prepare

--- prepare.sh
+++ prepare.sh
@@ -2,4 +2,5 @@
 cp docker-compose-sample.yml docker-compose.yml
 mkdir -p ./data ./initdb ./src/wordpress ./src/plugins ./src/themes ./src/uploads
 alias wp="docker-compose run --rm wpcli"
+vi docker-compose.yml
 docker-compose up -d
