8b1af20ab5dce45d3e797b89411878eb0b1bb0aa
Xavier Ruiz <xavier.ruiz@cronologic.com>
2018-11-07 19:19:56 +0100

    Fixed error on wp-install-sample.sh

--- wp-install-sample.sh
+++ wp-install-sample.sh
@@ -1,3 +1,3 @@
 #/bin/bash
 wp='docker-compose run --rm wpcli'
-$wp core install --url=localhost:8000 --title="Local Site" --admin_user=nimda --admin_password=secret --admin_email=admin@cronologic.com'
+$wp core install --url=localhost:8000 --title="Local Site" --admin_user=nimda --admin_password=secret --admin_email=admin@cronologic.com
